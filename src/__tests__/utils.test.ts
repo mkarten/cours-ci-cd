ff48f9a3ebf1358ff7b9232b999ca028343f4dc2
Luca Morgado <luca.morgado.pro@gmail.com>
2025-04-14 15:22:40 +0200

    pretier run

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,18 +1,21 @@
-import { createMessage, generateHtml } from '../utils';
+import { createMessage, generateHtml } from "../utils";
 
-describe('Utilitaires HTML', () => {
-  test('createMessage devrait générer un message HTML correctement', () => {
-    const message = 'Test Message';
-    const expected = '<div class="app__message">\n    <h1>Test Message</h1>\n</div>';
+describe("Utilitaires HTML", () => {
+  test("createMessage devrait générer un message HTML correctement", () => {
+    const message = "Test Message";
+    const expected =
+      '<div class="app__message">\n    <h1>Test Message</h1>\n</div>';
     expect(createMessage(message)).toBe(expected);
   });
 
-  test('generateHtml devrait générer un document HTML complet', () => {
-    const message = 'Hello Test';
+  test("generateHtml devrait générer un document HTML complet", () => {
+    const message = "Hello Test";
     const html = generateHtml(message);
-    expect(html).toContain('<title>My First Page</title>');
+    expect(html).toContain("<title>My First Page</title>");
     expect(html).toContain('<div class="app__message">');
-    expect(html).toContain('<h1>Hello Test</h1>');
-    expect(html).toContain('<link rel="stylesheet" href="/public/css/styles.css"/>');
+    expect(html).toContain("<h1>Hello Test</h1>");
+    expect(html).toContain(
+      '<link rel="stylesheet" href="/public/css/styles.css"/>',
+    );
   });
 });
